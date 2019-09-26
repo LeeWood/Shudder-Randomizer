a067513022ed7f8dda65aa11855f5ad52648df04
aleesha-wood <spectrat@spectratart.com>
2019-09-26 16:06:06 -0400

    added Button styled-components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Button from './Button.js';
+
+const WrapperDiv = styled.div`
+  font-family: sans-serif;
+  text-align center;
+`;
+
+const BlueText = styled.h1`
+  color: royalblue;
+`;
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WrapperDiv>
+      <BlueText>Styled Components Playground</BlueText>
+      <Button type="primary">Primary</Button>
+      <Button type="success">Success</Button>
+      <Button type="danger">Danger</Button>
+      <Button type="warning">Warning</Button>
+    </WrapperDiv>    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
@@ -11,4 +12,4 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
-//look at vid setup of bootstrap and documentation
+////look at vid setup of bootstrap and documentation
