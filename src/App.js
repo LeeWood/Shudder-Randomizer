f3c67c27987c6a310c2c158cfb83f0ceb2838bca
aleesha-wood <spectrat@spectratart.com>
2019-09-30 00:52:18 -0400

    figured out img sources, deleted unused files

--- src/App.js
+++ src/App.js
@@ -20,6 +20,10 @@ const BlueText = styled.h1`
 //   );
 // }
 
+const Smallimg = styled.img`
+  width: 200px;
+`;
+
 function App() {
   return (
     <WrapperDiv>
@@ -28,6 +32,8 @@ function App() {
       <exports.Button type="success" onClick={exports.badWord}>Success</exports.Button>
       <exports.Button type="danger" onClick={exports.danger}>Danger</exports.Button>
       <exports.Button type="warning">Warning</exports.Button>
+
+      <Smallimg src='https://previews.dropbox.com/p/thumb/AAkmKcx_tn2D_qP8hYoVyobkEQNYgE5dcLR6wUmm38fCl8U7XFHsisH0QDVuBGsGJ_xrpJnKvlhv8K95fSDePAXj3Tl7Cm0zLngXWewxtXxMiZue1ym3Y31rC2A14NmRsgL4qo9L7RKZoJ61QiDEOREicEO3vOblMoT_PBnSfCHHfDXAIjHAoT1TzGcRA7ObN0eOYyuxWC4DFQGmoGF28gWFLy9xVlpkMQJGtxL9JKb2GTcfWuH5lEJp5QL0IHCYsTHE8Ou6Doxnfp0vQhRTHDbZkIy-HBXROcOCw6ftN5Zbl9YyX-lAGUTpnz-agt9pF_ld6KLwIsQFrHycmtVIhBWyh9nVcLLfJwKNgArWsCe7qw/p.jpeg?fv_content=true&size_mode=5' alt='movie cover' />
     </WrapperDiv>    
   );
 }
---
+++ src/movie_data.js
@@ -0,0 +1,12 @@
+import { url } from "inspector"
+
+var MovieDate = [
+  {
+    title: "title2",
+    cover: "./movie_img/001.webp"
+  }
+]
+
+export default MovieDate;
+
+
