1dc3aa66f2acc17ea8bf8cd78abc2824452bee55
aleesha-wood <spectrat@spectratart.com>
2019-09-30 06:24:18 -0400

    removed reactstrap

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import styled from "styled-components";
-import { thing } from './components/MovieCard';
+import { thing, thingLink } from './components/MovieCard';
+
+const MovieImg = styled.img`
+  width: 250px;
+  margin-left: 500px;
+  margin-top: 100px;
+`;
+
+const Button = styled.button`
+  margin-left: 10px;
+  height: 40px;
+  justify-self: flex-end;
+`;
+
+const MainDiv = styled.div`
+  height: 70%;
+  display: flex;
+  align-items: center;
+`;
 
 function App() {
-  console.log (thing);
+  
+  const openInNewTab = (url) => {
+    var win=window.open(url, '_blank');
+    win.focus();
+  }
+  
   return (
-    <div>Content Goes Here!</div>
+    <MainDiv className="main-div">
+    <MovieImg src = {thing} alt="movie cover"/>
+    <Button onClick={() => {window.location.href=thingLink}}>Watch Now!</Button>
+    </MainDiv>
   );
-}
+};
+//this is a way to incorporate a button link that doesn't fire immediately as the DOM renders.
+//TODO find a way to get it to open in a new tab?
 
 export default App;
--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,8 +2,9 @@ import React from "react";
 import MovieData from "../movie_data.js";
 import styled from "styled-components";
 
-export const thing = 
-console.log(MovieData[2]);
+export const thing = MovieData[2].cover;
+export const thingLink = MovieData[2].url;
 
+//* OK...this is how to access the individual parts of the data file.
 
 
