80621e0ea80fad011c7dd36f369e0fb0512de9a3
aleesha-wood <spectrat@spectratart.com>
2019-10-02 01:55:56 -0400

    link redirection button(functional) and pick again (place holder) added

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import MovieData from "../movie_data.js";
 import styled from "styled-components";
+import {elemntVars} from "../pageElements.js"
 
 //styles//
 const CardDiv = styled.div`
@@ -14,32 +15,50 @@ const CardDiv = styled.div`
 `;
 const MovieCover = styled.img`
   height: 350px;
+  border-radius: 8px;
   
 `;
 const TextInfo = styled.div`
-  width: 350px;
+  width: 330px;
   align-self: flex-start;
 `;
-
 const TitlePara = styled.p`
   margin-top: 0;
   margin-bottom: 0;
   font-size: 1.75rem;
 `;
+const ButtonContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  button {
+    width: 125px;
+    height: 45px;
+    background-color: transparent; 
+    border: 2px solid ${elemntVars.mainRed};
+    border-radius: 15px;
+    color: ${elemntVars.mainRed};
+    font-family: ${elemntVars.featureFont};
+    font-size: 30px;
+  }
+    button:focus {
+      outline: transparent;
+      box-shadow: 1px 1px;
+    }
+`;
 
-let dataNum = MovieData.length;
 
 const randomNum = () => {
+  let dataNum = MovieData.length;
   return Math.floor(Math.random() * dataNum);
 }
 
 const randomMovie = MovieData[randomNum()]; 
+const visitPage = () => {
+  window.location=randomMovie.url;
+}
 
 const MovieCard = () => {
   
-  console.log(randomMovie.title);
-  console.log(randomNum());
-  console.log(MovieData[24]);
   return (
     <CardDiv>
       <MovieCover src={randomMovie.cover} />
@@ -52,6 +71,12 @@ const MovieCard = () => {
         <p className="info">
         Science fiction is often said to generate a "sense of wonder."  Science fiction editor and critic David Hartwell wrote: "Science fiction’s appeal lies in combination of the rational, the believable, with the miraculous. It is an appeal to the sense of wonder." Max Gladstone defined "hard" science fiction as stories "where the math works," but pointed out that this ends up with stories that often seem "weirdly dated," as scientific paradigms shift over time.
         </p>
+
+        <ButtonContainer>
+          <button type="submit" onClick={ () => {visitPage()}}>watch</button>
+          <button>pick again</button>
+
+        </ButtonContainer>
       </TextInfo>
     </CardDiv>
   );
